6350f670a9fa0cf4f640bb3403532cca1e6874f4
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 14:20:53 +0200

    add errors for checkout

---
+++ DigitalThinkers.Services/CashierService/Models/Errors/InconsistentCurrencyTypesError.cs
@@ -0,0 +1,14 @@
+﻿using DigitalThinkers.Common.Entities.Enumerations;
+using DigitalThinkers.Common.Entities.Models;
+
+namespace DigitalThinkers.CashierService.Models.Errors
+{
+    public class InconsistentCurrencyTypesError : BaseEvent
+    {
+        public InconsistentCurrencyTypesError(CurrencyType baseCurrency, CurrencyType inconsistentCurrency)
+        {
+            base.Title = "Inconsistent Currency Type";
+            base.Description = $"Inconsistent currencies provided. Base {baseCurrency}, but found: {inconsistentCurrency}!";
+        }
+    }
+}
---
+++ DigitalThinkers.Services/CashierService/Models/Errors/InvalidPriceError.cs
@@ -0,0 +1,13 @@
+﻿using DigitalThinkers.Common.Entities.Models;
+
+namespace DigitalThinkers.CashierService.Models.Errors
+{
+    public class InvalidPriceError : BaseEvent
+    {
+        public InvalidPriceError()
+        {
+            base.Title = "Invalid price";
+            base.Description = "Invalid price provided for checkout!";
+        }
+    }
+}

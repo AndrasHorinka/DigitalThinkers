31cdcce19f9c916c2e58b688d102aeb02ddec801
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 13:13:00 +0200

    refactor dataaccess main moved to subfolder

---
+++ DigitalThinkers.DataAccess/Main/Models/GetStockResult.cs
@@ -0,0 +1,10 @@
+﻿using DigitalThinkers.Common.Entities;
+using DigitalThinkers.Common.Entities.Models;
+
+namespace DigitalThinkers.DataAccess.Main.Models
+{
+    public class GetStockResult : Result
+    {
+        public Denominations AvailableStock { get; set; }
+    }
+}

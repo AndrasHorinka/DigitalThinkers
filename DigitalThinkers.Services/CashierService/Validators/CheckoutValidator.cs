9c4cae0bedfcaec2e00969e8105bd5603dcb80a5
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 14:22:09 +0200

    init Checkout Validator

---
+++ DigitalThinkers.Services/CashierService/Validators/CheckoutValidator.cs
@@ -0,0 +1,40 @@
+﻿using DigitalThinkers.CashierService.Models;
+using DigitalThinkers.CashierService.Models.Errors;
+using DigitalThinkers.Common.Entities.Extensions;
+using DigitalThinkers.Common.Entities.Models;
+using System.Linq;
+
+namespace DigitalThinkers.CashierService.Validators
+{
+    public class CheckoutValidator : BaseDenominationValidator
+    {
+        public Result Validate(CheckoutRequest request)
+        {
+            var result = new Result();
+
+            var baseValidationResult = base.Validate(request);
+            if (!baseValidationResult.IsSuccessful)
+            {
+                result.CopyErrorsFrom(baseValidationResult);
+                return result;
+            }
+
+            if (request.Price == 0)
+            {
+                result.Errors.Add(new InvalidPriceError());
+                return result;
+            }
+
+            foreach (var money in request.Denominations.MoneyInTransfer)
+            {
+                if (money.CurrencyType != request.CurrencyType)
+                {
+                    result.Errors.Add(new InconsistentCurrencyTypesError(request.CurrencyType, money.CurrencyType));
+                    return result;
+                }
+            }
+
+            return result;
+        }
+    }
+}

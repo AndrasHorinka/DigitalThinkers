3c2b7deb639e98ef42a587aa145918651eb650be
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 15:10:10 +0200

    added errors to stockUpResponse

--- DigitalThinkers.Web/Models/StockController/StockUpResponse.cs
+++ DigitalThinkers.Web/Models/StockController/StockUpResponse.cs
@@ -1,9 +1,16 @@
 ﻿using DigitalThinkers.Models.Common;
+using System.Collections.Generic;
 
-namespace DigitalThinkers.Models.StockController
+namespace DigitalThinkers.Models
 {
     public class StockUpResponse
     {
         public MoneyLoaded MoneyReturned { get; set; }
+        public IList<string> Errors { get; set; }
+
+        public StockUpResponse()
+        {
+            Errors = new List<string>();
+        }
     }
 }

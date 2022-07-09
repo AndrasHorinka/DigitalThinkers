a671f5303dab5263333ffa46b8b346435aa55f26
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 15:12:57 +0200

    toString override added to Result

--- DigitalThinkers.Common/DigitalThinkers.Common.Entities/Models/Result.cs
+++ DigitalThinkers.Common/DigitalThinkers.Common.Entities/Models/Result.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace DigitalThinkers.Common.Entities.Models
 {
@@ -11,6 +13,18 @@ namespace DigitalThinkers.Common.Entities.Models
         public Result()
         {
             this.Errors = new List<BaseEvent>();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in Errors)
+            {
+                sb.Append(item.Title);
+                sb.Append(Environment.NewLine);
+            }
+
+            return sb.ToString();
         }
     }
 }

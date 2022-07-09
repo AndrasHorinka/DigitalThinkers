43564f0f6021993a9637e6eccafcf48dd4bf802a
AndrasHorinka <horinka.andras@gmail.com>
2022-07-09 11:03:09 +0200

    init base endpoints for CheckoutController

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalThinkers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public CheckoutController(ILogger<CheckoutController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpPost]
+        public virtual ActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
